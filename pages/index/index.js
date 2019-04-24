f81e60200eecf4f63f7a09098b852685c862c7a0
wuwj-cn <wenjie.0617@gmail.com>
2019-04-25 00:00:00 +0800

    commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,50 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    list: [
+      {
+        id: 'view',
+        name: '视图容器',
+        open: false,
+        pages: ['view', 'scroll-view', 'swiper', 'movable-view', 'cover-view']
+      }, {
+        id: 'content',
+        name: '基础内容',
+        open: false,
+        pages: ['text', 'icon', 'progress', 'rich-text']
+      }, {
+        id: 'form',
+        name: '表单组件',
+        open: false,
+        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'picker-view', 'radio', 'slider', 'switch', 'textarea']
+      }, {
+        id: 'nav',
+        name: '导航',
+        open: false,
+        pages: ['navigator']
+      }, {
+        id: 'media',
+        name: '媒体组件',
+        open: false,
+        pages: ['image', 'audio', 'video', 'camera']
+      }, {
+        id: 'map',
+        name: '地图',
+        open: false,
+        pages: ['map']
+      }, {
+        id: 'canvas',
+        name: '画布',
+        open: false,
+        pages: ['canvas']
+      }, {
+        id: 'open',
+        name: '开放能力',
+        open: false,
+        pages: ['ad', 'open-data', 'web-view']
+      }
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
